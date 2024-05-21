ad1bb8c82011d22a2e3c010d8f3f458f92cfc473
vol2468 <vol2468@student.ubc.ca>
2024-05-21 14:23:40 -0700

    created templates

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,8 +1,20 @@
 # from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-from django.http import HttpResponse
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class SigninView(TemplateView):
+    template_name = 'signin.html'
+
+class SignupView(TemplateView):
+    template_name = 'signup.html'
+
+class AccountView(TemplateView):
+    template_name = 'account.html'
