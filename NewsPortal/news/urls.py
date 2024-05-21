fb61b90e2af4f60f953ae4e9d361012cef35606c
vol2468 <vol2468@student.ubc.ca>
2024-05-21 14:23:40 -0700

    created templates

--- NewsPortal/news/urls.py
+++ NewsPortal/news/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+# from . import views
+from .views import IndexView, SigninView, SignupView, AccountView
 
-from . import views
+# urlpatterns = [
+#     path('', views.index, name="index"),
+# ]
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', IndexView.as_view()),
+    path('signin', SigninView.as_view()),
+    path('signup', SignupView.as_view()),
+    path('account', AccountView.as_view()),
 ]
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# News-portal
+# News Portal Website
