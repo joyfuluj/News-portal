17104a27a8fe8eff5b3ef5aac0ef6b328b0cd725
joyfuluj <joyumejiego@yahoo.com>
2024-05-26 21:27:51 -0700

    added conditions when getting info from API

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -34,6 +35,29 @@ class NewaportalView(APIView):
                'apiKey=b92aefe2bfb44f199bd1964fb09fa8d7')
         response = requests.get(url)
         data = response.json()
-        status = data.get('status')
-        return render(request, 'index.html', {'status': status})
-        # return Response(status)
+        articles = data.get('articles')
+        detail = self.getDetails(articles)
+        return render(request, 'index.html', {'status': detail})
+        # return Response(detail)
+    
+    def getDetails(self, articles):
+        detail_list = []
+        for article in range(1,len(articles)):
+            item = articles[article]
+            date_published = item.get('publishedAt')
+            date_part = ""
+            if date_published:
+                datetime_obj = datetime.strptime(date_published, "%Y-%m-%dT%H:%M:%SZ")
+                date_part = datetime_obj.date()
+            detail = {
+                'source' : item.get('source').get('name'),
+                'title' : item.get('title'),
+                'url' : item.get('url'),
+                'image_url' : item.get('urlToImage'),
+                'content' : item.get('content'),
+                'date_published' : date_part,
+            }
+            if detail.get('source') != "[Removed]" and detail.get('content'):
+                detail_list.append(detail)
+        return detail_list
+    
